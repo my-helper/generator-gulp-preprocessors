5ef5fc899cd81eca72741ac62990473f3dfb38b9
pure-js <evolution.is.already.underway@gmail.com>
2016-11-15 19:14:22 +0300

    add user interaction

--- app/index.js
+++ app/index.js
@@ -1,10 +1,25 @@
-let generators = require('yeoman-generator');
+const generators = require('yeoman-generator');
 
 module.exports = generators.Base.extend({
-	method1: function () {
-		console.log('method 1 just ran');
-	},
-	method2: function () {
-		console.log('method 2 just ran');
-	}
+  prompting: function () {
+    return this.prompt([{
+      type: 'input',
+      name: 'name',
+      message: 'Your project name',
+      default: this.appname
+    }, {
+      type: 'confirm',
+      name: 'cool',
+      message: 'Would you like to enable the Cool feature?'
+    }]).then(function (answers) {
+      this.log('app name', answers.name);
+      this.log('cool feature', answers.cool);
+    }.bind(this));
+  },
+  method1: function () {
+    console.log('method 1 just ran');
+  },
+  method2: function () {
+    console.log('method 2 just ran');
+  }
 });
